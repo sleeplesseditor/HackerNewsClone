6ee7548a2ccc3d8c3cfcb0c831a2fd69af2f2e52
sleeplesseditor <josh@bryson.me.uk>
2019-01-31 00:53:57 +0000

    Article links setup

--- components/StoryList.js
+++ components/StoryList.js
@@ -1,7 +1,19 @@
+import Link from 'next/link';
+
 const StoryList = ({ stories }) => (
-    <div>
+    <div className="story-list">
         {stories.map(story => (
-            <h3 key={story.id}>{story.title}</h3>
+            <div key={story.id} className="story">
+                <h2 className="story-title">
+                    <a href={story.url}>{story.title}</a>
+                </h2>
+                <div className="story-detail">
+                    <span>{story.points || 0} points</span>
+                    <Link href={`/story?id=${story.id}`}>
+                        <a>{story.comment_count || 0} comments</a>
+                    </Link>
+                </div>
+            </div>
         ))}
     </div>
 );
