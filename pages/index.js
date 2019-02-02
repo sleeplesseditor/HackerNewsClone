3f5de868b4a2e88d014ca67713d48d1da06bd609
sleeplesseditor <josh@bryson.me.uk>
2019-02-02 10:32:19 +0000

    Pagination and additional styling added

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,63 @@
 import React, { Component } from 'react';
 import fetch from 'isomorphic-fetch';
 import Error from 'next/error';
-
+import Link from 'next/link';
+import Layout from '../components/Layout';
 import StoryList from '../components/StoryList';
 
 class Index extends Component {
-    static async getInitialProps() {
+    static async getInitialProps({ req, res, query }) {
         let stories;
+        let page;
 
         try {
-            const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+            page = Number(query.page) || 1;
+            const response = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
             stories = await response.json();
         } catch (err) {
             console.log(err);
             stories = [];
         }
 
-        return { stories };
+        return { page, stories };
     }
 
     render () {
-        const { stories } = this.props;
+        const { page, stories } = this.props;
 
         if(stories.length === 0) {
             return <Error statusCode={503} />;
         }
 
         return (
-            <div>
-                <h1>HackerNews Clone</h1>
+            <Layout title="HackerNews Clone">
                 <StoryList stories={stories} />
-            </div>
+
+                <button>
+                    <Link href={`/?page=${page + 1}`}>
+                        <a>Load More...</a>
+                    </Link>
+                </button>
+
+                <button>
+                    <Link href={`/?page=${page - 1}`}>
+                        <a>Previous</a>
+                    </Link>
+                </button>
+
+                <style jsx>
+                    {`
+                        button {
+                            font-size: 24px;
+                            font-weight: 600;
+                        }
+
+                        button a {
+                            text-decoration: none;
+                        }
+                    `}
+                </style>
+            </Layout>
         );
     }
 }
